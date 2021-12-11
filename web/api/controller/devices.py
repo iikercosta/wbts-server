d0bd93434b23db5f023c1b34adc074e1313d35a7
ikerh <ikerhabib@gmail.com>
2021-12-12 00:39:03 +0100

    Added devices controlling funtion: stats route

--- web/api/controller/devices.py
+++ web/api/controller/devices.py
@@ -1,14 +1,13 @@
 import flask
 from flask import Blueprint, request
 
-from web.api.service.devices_service import register_devs
+from web.api.service.devices_service import register_devs, get_devices_by_type
 
 devices = Blueprint('devices', __name__)
 
 
 @devices.route('/register-devices', methods=['POST'])
 def register_devices():
-
     data = request.get_json()
 
     if data:
@@ -18,6 +17,11 @@ def register_devices():
         return flask.Response(response="400")
 
 
+@devices.route('/get-devices', methods=['GET'])
+def get_devices():
 
+    dev_type = request.args.get('dev_type', type=str)
 
+    data = get_devices_by_type(dev_type)
 
+    return flask.Response(data, mimetype="text/csv", headers={"Content-disposition": "attachment;filename=data.csv"})
